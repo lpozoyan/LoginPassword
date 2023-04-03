f97b75849d7b4ade24bdbb4bb18b09eb10f40a16
stud <stud@107-5>
2023-04-03 08:10:20 +0400

    Добавьте файлы проекта.

---
+++ AuthService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LoginPassword
+{
+    internal class AuthService
+    {
+        public bool IsValid(string login, string password)
+        {
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            {
+                throw new System.ArgumentException("Логин или пароль пустые");
+            }
+            if (login == "adm" && password == "132")
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginPassword
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            AuthService authService = new AuthService();
+            string login = tbLogin.Text;
+            string password = tbPassword.Text;
+            try
+            {
+                bool valid = authService.IsValid(login, password);
+                if (valid == true)
+                {
+                    Window1 loginPassed = new Window1();
+                    loginPassed.Show();
+                    this.Close();
+                }
+                else 
+                { 
+                    var mb = MessageBox.Show("Неверный логин или пароль", "ошибка", MessageBoxButton.YesNo);
+                    if(mb == MessageBoxResult.No)
+                   
+                } 
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Сообщение",MessageBoxButton.YesNo, MessageBoxImage.Error);
+            }
+            
+        }
+    }
+}
